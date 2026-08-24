4f5b17f087ef1b6366264bcded3e404e02a97ed9
cozyspot-azurestaycation <cozyspotstaycation@gmail.com>
2026-08-24 23:52:47 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-
 import { Hero } from "@/components/hero";
 import { ExperiencePicker } from "@/components/experience-picker";
 import { SpaceShowcase } from "@/components/space-showcase";
-import { UnitDescription } from "@/components/unit-description";
 import { Amenities } from "@/components/amenities";
 import { Trust } from "@/components/trust";
 import { Reviews } from "@/components/reviews";
@@ -13,9 +11,22 @@ export default function HomePage() {
   return (
     <>
       <Hero />
+
       <ExperiencePicker />
+
       <SpaceShowcase />
-      <UnitDescription />
+
       <div id="amenities">
         <Amenities />
       </div>
+
+      <Trust />
+
+      <Reviews />
+
+      <LocationSection />
+
+      <BookingCta />
+    </>
+  );
+}
