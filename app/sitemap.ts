adaae3086d6519176982254d515c784bd2694b04
cozyspot-azurestaycation <cozyspotstaycation@gmail.com>
2026-09-01 13:56:55 +0800

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 import { siteConfig } from "@/lib/site-config";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ["", "/faq"];
+  const routes = ["", "/faq", "/gallery", "/unit-inclusions"];
 
   return routes.map((route) => ({
     url: `${siteConfig.url}${route}`,
